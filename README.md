ddc0cf9de866cd34848259c6413bfd14e11ce2ff
Glauco Pater <>
2020-07-17 20:40:53 +0200

    Init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Photo Portfolio - Gatsby + Contentful + Netlify
+
+[See it in action](https://jpvalery.photo)
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/592d351c-5180-41c9-be77-e98fbc33cd2e/deploy-status)](https://app.netlify.com/sites/jpvalery-portfolio/deploys) [![Maintainability](https://api.codeclimate.com/v1/badges/8c4c1cfe4b182212ae3c/maintainability)](https://codeclimate.com/github/jpvalery/portfolio/maintainability) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/e4864e257d8d4c40a2a3535b619f9fe9)](https://www.codacy.com/app/jpvalery/portfolio?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=jpvalery/portfolio&amp;utm_campaign=Badge_Grade)
+
+
+## Overview
+
+Codebase for [https://jpvalery.photo](https://jpvalery.photo) my photography portfolio.
+Current iteration is built using Contentful, Netlify, and Gatsby V2; and was launched in January 2019. Based on [@iammatthias/.com](https://github.com/iammatthias/.com)
+
+This site makes heavy use of the [`gatsby-image`](https://next.gatsbyjs.org/packages/gatsby-image/) plugin, pulling optimized fluid assets from Contentful.
+
+### Features
+
+- Netlify integration
+- Contentful integration
+- Analytics with [Fathom](https://usefathom.com)
+- SEO
+- Styled components
+- RSS Feed
+
+##### Notes
+
+> Code can be used as a reference for other Gatsby projects, but this repo will not work out-of-the-box without replicating the Contentful data structure. If you would like to quickly bootstrap a Gatsby + Contentful + Netlify project, I recommend checking out the fantastic [GCN](https://github.com/ryanwiemer/gatsby-starter-gcn) starter.
+
+> This repo uses Yarn, over NPM.
---
+++ src/pages/index.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import WrapperGrid from '../components/WrapperGrid'
+import HomeBody from '../components/Home/HomeBody'
+import HomeBodyBottom from '../components/Home/HomeBodyBottom'
+import HomeList from '../components/Home/HomeList'
+
+import SEO from '../components/SEO'
+
+const Index = ({ data, location }) => {
+  const galleries = data.allContentfulExtendedGallery.edges
+  return (
+    <Layout location={location}>
+      <SEO />
+      <WrapperGrid>
+        <HomeBody>
+          <HomeBodyBottom>
+            {galleries.map(({ node: gallery }) => (
+              <HomeList
+                key={gallery.id}
+                slug={gallery.slug}
+                image={gallery.heroImage}
+                title={gallery.title}
+                year={gallery.year}
+                tags={gallery.tags}
+                date={gallery.publishDate}
+                excerpt={gallery.body}
+              />
+            ))}
+          </HomeBodyBottom>
+        </HomeBody>
+      </WrapperGrid>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query Index {
+    allContentfulExtendedGallery(limit: 20) {
+      edges {
+        node {
+          title
+          id
+          slug
+          heroImage {
+            title
+            fluid(maxWidth: 1000) {
+              ...GatsbyContentfulFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+    contentfulPost {
+      title
+      id
+      heroImage {
+        title
+        fluid(maxWidth: 1000) {
+          ...GatsbyContentfulFluid_withWebp
+        }
+        ogimg: resize(width: 900) {
+          src
+          width
+          height
+        }
+      }
+      body {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+  }
+`
+
+export default Index
